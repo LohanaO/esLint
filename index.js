182cd37e6971d7f7d45f7191c9519e8a69e76e00
Lohana <Loha@hotmail.com>
2023-03-25 18:08:51 +0100

    primer commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// Mi nombre
+let nombre="Lohana"
+// Mi Apellido
+let apellido= "Orellano"
+
+let estudiante=nombre.concat(" ",apellido)
+
+
+// eslint-disable-next-line no-unused-vars
+let estudianteMayus= estudiante.toUpperCase()
+
+
+// eslint-disable-next-line no-unused-vars
+let estudianteMinus= estudiante.toLowerCase()
+
+
+// eslint-disable-next-line no-unused-vars
+let est_length = estudiante.length
+
+
+// eslint-disable-next-line no-unused-vars
+let primera_nombre= nombre[0]
+
+
+let apellido_length= apellido.length
+ 
+// eslint-disable-next-line no-unused-vars
+let ult_ape= apellido[apellido_length-1]
+
+
+// eslint-disable-next-line no-unused-vars
+let Espacios= estudiante.replace(/ /g, "")
+
+
+// eslint-disable-next-line no-unused-vars
+let busc_nombre= estudiante.includes(nombre)
